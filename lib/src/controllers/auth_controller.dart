47b38b3eb9d895eaf3069a9358a56d2c0328787b
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-06-28 04:20:42 +0800

    added log in function

--- lib/src/controllers/auth_controller.dart
+++ lib/src/controllers/auth_controller.dart
@@ -18,12 +18,12 @@ class AuthController with ChangeNotifier {
   static AuthController get I => GetIt.instance<AuthController>();
 
   AuthState state = AuthState.unauthenticated;
-  SimulatedAPI api = SimulatedAPI();
+  // SimulatedAPI api = SimulatedAPI();
   late StreamSubscription<User?> currentAuthedUser;
 
   listen() {
     currentAuthedUser =
-        FirebaseAuth.instance.userChanges().listen(handleUserChanges);
+        FirebaseAuth.instance.authStateChanges().listen(handleUserChanges);
   }
 
   void handleUserChanges(User? user) {
@@ -36,37 +36,32 @@ class AuthController with ChangeNotifier {
   }
 
   login(String userName, String password) async {
-    bool isLoggedIn = await api.login(userName, password);
-    if (isLoggedIn) {
-      state = AuthState.authenticated;
-      //should store session
-
-      notifyListeners();
-    }
+    UserCredential userCredential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: userName, password: password);
   }
 
   ///write code to log out the user and add it to the home page.
-  logout() {
-    //should clear session
-  }
+  logout() {}
 
   ///must be called in main before runApp
   ///
   loadSession() async {
-    //check secure storage method
+    listen();
+    User? user = FirebaseAuth.instance.currentUser;
+    handleUserChanges(user);
   }
 
   ///https://pub.dev/packages/flutter_secure_storage or any caching dependency of your choice like localstorage, hive, or a db
 }
 
-class SimulatedAPI {
-  Map<String, String> users = {"testUser": "12345678ABCabc!"};
+// class SimulatedAPI {
+//   Map<String, String> users = {"testUser": "12345678ABCabc!"};
 
-  Future<bool> login(String userName, String password) async {
-    await Future.delayed(const Duration(seconds: 4));
-    if (users[userName] == null) throw Exception("User does not exist");
-    if (users[userName] != password)
-      throw Exception("Password does not match!");
-    return users[userName] == password;
-  }
-}
+//   Future<bool> login(String userName, String password) async {
+//     await Future.delayed(const Duration(seconds: 4));
+//     if (users[userName] == null) throw Exception("User does not exist");
+//     if (users[userName] != password)
+//       throw Exception("Password does not match!");
+//     return users[userName] == password;
+//   }
+// }
